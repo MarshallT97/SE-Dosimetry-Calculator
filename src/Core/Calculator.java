acb6d43b8486fadba7fec97b2e6e4a2337e9d0d2
Be3N2 <2017WakefieldB@gmail.com>
2019-10-06 23:16:29 -0400

    Initial Setup

---
+++ src/Core/Calculator.java
@@ -0,0 +1,56 @@
+package Core;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Router
+ */
+@WebServlet("/Calculator")
+public class Calculator extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Calculator() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter writer = response.getWriter();
+		
+		//Get Inputted String From Header
+		String inputString = request.getHeader("basic");
+		String emptyString = "";
+		//Reverse the string (copying into EmptyString)
+		for (int i = 0; i < inputString.length(); i++) {
+			int index = inputString.length()-(1+i);
+			emptyString += inputString.charAt(index);
+		}
+		
+		//Return the new string
+		writer.println(emptyString);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
